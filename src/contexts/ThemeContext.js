99639735405843a89ee5d3686baf7706e289164f
Hanzla Baig <hanzlabaig917@gmail.com>
2025-11-01 09:35:50 +0500

    Create ThemeContext.js

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext } from 'react';
+import { useTheme } from '../hooks/useTheme';
+
+const ThemeContext = createContext();
+
+export const useThemeContext = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useThemeContext must be used within a ThemeProvider');
+  }
+  return context;
+};
+
+export const ThemeProvider = ({ children }) => {
+  const theme = useTheme();
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
